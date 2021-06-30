3b3b5deef1641a05b890239358d36cc75b956ec1
PabloLomeli <pablolomeli@u.boisestate.edu>
2021-06-29 19:52:47 -0600

    added dialog button using fragments

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
 
-], function(Controller, MessageToast) {
+], function(Controller, MessageToast, Fragment) {
     'use strict';
     
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
@@ -14,6 +15,26 @@ sap.ui.define([
             
             // Show a native or vanilla JS alert
             MessageToast.show(sMsg);
+        },
+
+        onOpenDialog : function () {
+            var oView = this.getView();
+            
+            //create the dialog using frag
+            if(!this.byId("helloDialog")) {
+                // async load of dialog frag
+                Fragment.load({
+                    id: oView.getId(),
+                    name: "sap.ui.demo.walkthrough.view.HelloDialog"
+                }).then(function (oDialog) {
+                    // connect dialog to root view of component (models, lifecycle)
+                    oView.addDependent(oDialog);
+                    oDialog.open();
+                });
+            } else {
+                this.byId("helloDialog").open();
+            }
+
         }
     });
 });
